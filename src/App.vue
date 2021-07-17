35b680ae5f0a1dfbdd12201f67674bb3beb5df8b
Leahhung <leahung1119@gmail.com>
2021-07-17 15:57:18 +0800

    (firststep)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
+    <navBar/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <rightSide/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import navBar from './components/navbar.vue'
+import rightSide from './components/rightside.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    navBar,
+    rightSide
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "/src/assets/all.scss"
 </style>
---
+++ src/components/rightside.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="rightside">
+    <div class="rightside">
+      <div class="rightside-up circle">
+        線上開戶
+      </div>
+      <div class="rightside-down circle">
+        加開證券戶
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+
+</style>

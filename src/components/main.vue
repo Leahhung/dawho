18d51b08c08f281449794cb4ef89a232051f19f4
yuting1119 <leahung1119@gmail.com>
2021-08-23 15:10:04 +0800

    main(pc)

--- src/components/main.vue
+++ src/components/main.vue
@@ -42,52 +42,71 @@
         <img src="../assets/kv_02_img.jpeg" alt="" data-aos="fade-left" data-aos-delay="200">
       </div>
       <div class="item3">
-        <img src="../assets/kv_06_img.jpeg" alt="" data-aos="fade-right" data-aos-delay="200">
-        <div class="item3-right">
-          <div class="l">放<span class="red-l">大</span>你的夢想</div>
+        <img src="../assets/kv_06_img.jpeg" alt="" data-aos="fade-right">
+        <div class="item3-right" data-aos="fade-right" data-aos-delay="200">
+          <div class="l" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">放<span class="red-l">大</span>你的夢想</div>
           <div class="s">早上進件，下午領現</div>
           <div class="m">利率最低<span class="percent">0.1%</span>起</div>
-          <p class="item2-btn btn">了解更多＞</p>
+          <p class="item3-btn btn">了解更多＞</p>
         </div>
       </div>
       <div class="item4">
-        <div class="item4-left">
-          <div class="l">壯<span class="red-l">大</span>你的投資</div>
+        <div class="item4-left" data-aos="fade-right" data-aos-delay="200">
+          <div class="l" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">壯<span class="red-l">大</span>你的投資</div>
           <div class="s">多省即先賺 不浪費重要的每一分錢！</div>
           <div class="m">下單永豐ibrAin即想帳管費<span class="percent">0元</span></div>
           <div class="m">精選基金申購手續費<span class="percent">0元</span></div>
+          <p class="item4-btn btn">了解更多＞</p>
         </div>
         <img src="../assets/kv_05_img.jpeg" alt="" data-aos="fade-left" data-aos-delay="200">
       </div>
-      <div class="item5"></div>
+      <div class="item5">
+        <img src="../assets/kv_04_img.jpeg" data-aos="fade-right" alt="">
+        <div class="item5-right" data-aos="fade-left" data-aos-delay="200">
+          <div class="l" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1000">擴<span class="red-l">大</span>你的便利</div>
+          <div class="s">想轉就轉 想提就提 毫無顧忌</div>
+          <div class="m">跨行提款＆轉帳 每月免費共<span class="percent">20次</span></div>
+          <div class="red-s">每月優惠減免次數期間為當月16日~次月15日(優惠僅跨行提款及跨行轉帳，不含繳費、跨行存款等)</div>
+          <p class="item4-btn btn">了解更多＞</p>
+        </div>
+      </div>
     </main>
     <footer>
-      <div class="footer up">
-        <p>千呼萬喚，質感上市</p>
+      <div class="footer-up">
+        <p class="footer-main-title">千呼萬喚，質感上市</p>
         <div class="footer-main">
-          <img src="../assets/footer_download.png" alt="">
-          <p>三大特色功能搶先體驗</p>
-          <p>帳務利息，天天更新 ｜ 消費回饋，輕鬆查詢 ｜ 安全簡易，無碼轉帳</p>
-          <div>
+          <div class="footer-main-left">
+            <img src="../assets/footer_download.png" alt="">
+          </div>
+          <div class="footer-main-right">
+          <p class="p1">三大特色功能搶先體驗</p>
+          <p class="p2">帳務利息，天天更新 <span>｜</span> 消費回饋，輕鬆查詢 <span>｜</span> 安全簡易，無碼轉帳</p>
+          <div class="footer-main-right-down">
             <img src="../assets/download_qrcode.png" alt="">
-            <div class="download_content">
-              <p>立即下載DAWHO</p>
-              <p>最新版本號：iOS、Android 6</p>
-              <p>最低支援作業系統：iOS 11.0(含)以上、Android 5.0(含)以上</p>
+            <div class="download-content">
+              <p class="p-title">立即下載 DAWHO</p>
+              <p class="p-content">最新版本號：iOS、Android 6<br>最低支援作業系統：iOS 11.0(含)以上、Android 5.0(含)以上</p>
+              <div class="download-btn">
+                <img class="apple-btn" src="https://dawho.tw/img/index/download_apple.png" alt="apple store">
+                <img class="google-btn" src="https://dawho.tw/img/index/download_google.png" alt="google play">
+              </div>
             </div>
+          </div>
           </div>
         </div>
       </div>
-      <div class="footer down">
-        <div class="down-left">謹慎理財 信用至上</div>
-        <div>
-          <p>24小時服務專線：(02)2528-7776/0800-058-888 (限市話)</p>
-          <p>循環信用利率年息2.74%~15% (基準日2015/8/3)，預借現金手續費：預借現金金額X3.5%+指定金額</p>
-          <p>(100元新台幣/3.5美元/350日圓/3歐元) 其他費用請上永豐銀行網站查詢</p>
+      <div class="footer-down">
+        <div class="down-container">
+          <div class="down-left">謹慎理財 信用至上</div>
+          <div class="down-right">
+            <div class="down-right-content">
+              <p>24小時服務專線：(02)2528-7776/0800-058-888 (限市話)</p><p>循環信用利率年息2.74%~15% (基準日2015/8/3)，預借現金手續費：預借現金金額X3.5%+指定金額</p><p>(100元新台幣/3.5美元/350日圓/3歐元) 其他費用請上永豐銀行網站查詢</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>©版權所有  <img src="../assets/footer_logo1.jpg" alt="">永豐銀行</p>
-          <p><img src="../assets/footer_logo2.jpg" alt="">存款人存保權益 法定公告事項 隱私權保護聲明</p>
+        <div class="down-copyright">
+          <p class="p1">©版權所有  <img class="img1" src="https://www.logosurfer.com/wp-content/uploads/2018/03/Bank20SinoPac.png" alt=""> 永豐銀行</p>
+          <p class="p2"><img src="https://dawho.tw/img/cdic.png" alt=""> 存款人存保權益 法定公告事項 隱私權保護聲明</p>
         </div>
       </div>
     </footer>
